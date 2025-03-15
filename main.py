0df7b93b36832f93bd5f4421261faf5044eeb062
RohitPawar001 <rppawar491@gmail.com>
2025-03-15 22:08:35 +0530

    structure

---
+++ main.py
@@ -0,0 +1,10 @@
+print("hellow")
+
+from resys.data_source import h_and_m_data
+
+
+def mainn():
+    #data = extract_articles_df()
+    print("Hello from h-and-m-recommendation-system!")
+
+mainn()
