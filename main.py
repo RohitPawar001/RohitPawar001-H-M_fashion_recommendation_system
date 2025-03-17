11c5be1b3e60defaa721578369dae2617d0bf9ca
RohitPawar001 <rppawar491@gmail.com>
2025-03-17 21:15:40 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from resys.data_source.h_and_m_data import extract_articles_df
+
+
 def main():
+    
+    print("hi")
+    #extract_articles_df()
     print("Hello from h-and-m-recommendation-system!")
 
 
